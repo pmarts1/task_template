208be6f430fe34fc1968ab2c7493eaee6baa5274
pm1 <pasha31032004@gmail.com>
2022-11-01 17:35:56 +0300

    1

---
+++ 11.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def th(arr, threshold, width):
+    l = []
+    a = arr
+    b = np.array([0 for i in range(len(a))])
+    for i in range(len(a)):
+        if a[i] > threshold:
+            b[i] = 1
+            for j in range(i-int(width/2), i+int(width/2)+1):
+                if j >= 0 and j < len(a):
+                    if a[j] > threshold and j != i:
+                        b[i] = 2;
+    for i in range(len(b)):
+        if b[i] == 1:
+            l.append(i)
+    r
